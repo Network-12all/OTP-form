310c9622572657917781dd553823b6404f149241
Okolo kamdilichukwu samuel <125130084+Network-12all@users.noreply.github.com>
2023-08-14 04:04:33 -0400

    script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+      
+      const inputs = document.querySelectorAll("input"),
+      button = document.querySelectorAll("button");
+      
+      inputs.forEach((input, index1)=> {
+        input.addEventListener("keyup", (e)=>{
+          
+          const currentInput = input,
+             nextInput = input.nextElementSibling,
+             prevInput = input.previousElementSibling;
+             
+             if (currentInput.value.length > 1){
+               currentInput.value = "";
+               return;
+             }
+             
+             if (nextInput && nextInput.hasAttribute("disabled") && currentInput.value !== ""){
+               nextInput.removeAttribute("disabled");
+               nextInput.focus();
+             }
+        })
+      })
+    
